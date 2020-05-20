e0298a74d9be5e20224a7bd563178b08123f64d6
Timbab <timbab2@gmail.com>
2020-05-20 15:58:07 +0200

    Added WIP UtinniCoreDotNetGen -- interop generator using CppSharp

---
+++ UtinniCoreDotNetGen/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using CppSharp;
+using CppSharp.AST;
+using CppSharp.Generators;
+
+namespace UtinniCoreDotNetGen
+{
+    class Program
+    {
+        class Gen : ILibrary
+        {
+            public void Setup(Driver driver)
+            {
+                string workingDir = AppDomain.CurrentDomain.BaseDirectory;
+                string buildMode = new DirectoryInfo(workingDir).Name;
+                string slnDir = Directory.GetParent(workingDir.Substring(0, workingDir.LastIndexOf("\\bin\\"))).FullName + "\\"; 
+
+                driver.ParserOptions.TargetTriple = "i686-pc-win32-msvc"; // Generates x86 EntryPoints
+                var options = driver.Options;
+                options.GeneratorKind = GeneratorKind.CSharp;
+                options.OutputDir = slnDir + "UtinniCoreDotNet\\Generated\\";
+
+                string targetProjName = "UtinniCore";
+                var module = options.AddModule(targetProjName);
+
+                // Includes
+                module.IncludeDirs.Add(slnDir + targetProjName); // ToDo make this a loop to grab all the subfolders
+
+                // Headers
+                //module.Headers.Add("world_snapshot.h");
+
+                // Library
+                module.LibraryDirs.Add(slnDir + "bin\\" + buildMode + "\\");
+                module.Libraries.Add(targetProjName + ".dll");
+            }
+
+            public void SetupPasses(Driver driver)
+            {
+            }
+
+            public void Preprocess(Driver driver, ASTContext ctx)
+            {
+                ctx.IgnoreHeadersWithName("utinni");
+
+            }
+
+            public void Postprocess(Driver driver, ASTContext ctx)
+            {
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            ConsoleDriver.Run(new Gen());
+        }
+    }
+}
---
+++ UtinniCoreDotNetGen/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UtinniCoreDotNetGen")]
+[assembly: AssemblyDescription(".NET Generator for UtinniCore")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Philip Klatt")]
+[assembly: AssemblyProduct("UtinniCoreDotNetGen")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88a2edd9-fbe1-4d66-a867-6359b31566d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
