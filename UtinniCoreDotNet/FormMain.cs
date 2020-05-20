6a0bc4928e09987fe143b8dd05fcf8adce1314a0
Timbab <timbab2@gmail.com>
2020-05-20 14:37:38 +0200

    Added UtinniCoreDotNet

---
+++ UtinniCoreDotNet/FormMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+using UtinniCore;
+
+namespace UtinniCoreDotNet
+{
+    public partial class FormMain : Form
+    {
+		
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+			
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+			
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ UtinniCoreDotNet/main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace UtinniCoreDotNet
+{
+    internal static class Startup
+    {
+        private static bool m_initialized;
+		
+        [STAThread]
+        private static int EntryPoint(string args)
+        {
+            if (!m_initialized)
+            {
+                m_initialized = true;
+                Application.EnableVisualStyles();
+				
+				// Test Form
+                Application.Run(new FormMain());
+            }
+            return 0;
+        }
+
+
+    }
+}
