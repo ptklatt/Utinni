273d42e27e822a3544e9b97f9cb6d4b36a11d9d9
ptklatt <timbab2@gmail.com>
2020-08-14 22:55:48 +0200

    UtinniCore -- WIP swg_new issues

--- UtinniCore/swg/misc/swg_utility.h
+++ UtinniCore/swg/misc/swg_utility.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "utinni.h"
+#include <new>
 
 namespace utinni
 {
@@ -9,4 +10,38 @@ extern DWORD allocateMemory(size_t size);
 extern unsigned int calculateCrc(const char* string);
 
 extern DWORD treeFileOpen(const char* filename, int priorityType, bool allowFail);
+
+}
+
+namespace utinni
+{
+struct NewPlaceholder {};
+}
+
+inline void* operator new(size_t, utinni::NewPlaceholder, void* where)
+{
+    return where;
+}
+
+inline void operator delete(void*, utinni::NewPlaceholder, void*)
+{
+}
+
+namespace utinni
+{
+//template<class Type, class... Args>
+//Type* swg_new(Args&&...args)
+//{
+//    //return new (NewPlaceholder(), allocateMemory(sizeof(Type)), ctor) Type ctor();
+//    
+//    return new (sizeof(Type), NewPlaceholder(), allocateMemory(sizeof(Type))) Type(std::forward<Args>(args)...);
+//}
+//
+template<class Type>
+Type* swg_new()
+{
+    //return new (NewPlaceholder(), allocateMemory(sizeof(Type)), ctor) Type ctor();
+
+    return new (NewPlaceholder(), allocateMemory(sizeof(Type))) Type;
+}
 }
