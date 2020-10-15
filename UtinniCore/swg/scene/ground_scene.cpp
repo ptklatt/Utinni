ptklatt/Utinni

commit 91366e560740cfe4b731d1a6b504fa9202465017

UtinniCore -- Added a callable collide function to cui_hud to be able to get the world pos from the cursor

UtinniCore/swg/scene/ground_scene.cpp
@@ -53,16 +53,20 @@ GroundScene* GroundScene::ctor(const char* terrainFilename, const char* avatarOb
 
 std::string GroundScene::getName()
 {
-    std::string terrainPath = Terrain::get()->getFilename();
+    const std::string terrainPath = Terrain::get()->getFilename();
 
     if (terrainPath.empty())
+    {
         return "";
+    }
 
     int i = terrainPath.find_first_of('/') + 1;
     const int length = terrainPath.size() - i - 5;
 
     if (length < 0)
+    {
         return "";
+    }
 
     return terrainPath.substr(i, length);
 }
@@ -100,12 +104,13 @@ void GroundScene::addUpdateLoopCallback(void(*func)(GroundScene* pThis, float el
 
 void __fastcall hkUpdateLoop(utinni::GroundScene* pThis, DWORD EDX, float time)
 {
+  
+    swg::groundScene::update(pThis, time);
     for (const auto& func : updateLoopCallbacks)
     {
         func(pThis, time);
     }
 
-    swg::groundScene::update(pThis, time);
 }
 
 void GroundScene::detour()

UtinniCore/swg/game/game.cpp
@@ -1,10 +1,12 @@
 #include "game.h"
 #include "utinni.h";
+#include <imgui/imgui_user.h>
 #include "swg/client/client.h"
 #include "swg/misc/config.h"
-#include "swg/object/object.h"
 #include "swg/scene/ground_scene.h"
 #include "swg/scene/world_snapshot.h"
+#include "swg/object/client_object.h"
+#include "swg/object/object.h"
 #include "swg/ui/imgui_impl.h"
 
 namespace swg::game
@@ -217,7 +219,9 @@ swgptr Game::getPlayerLookAtTargetObjectNetworkId()
     const Object* playerObj = getPlayerCreatureObject();
 
     if (!playerObj)
+    {
         return 0;
+    }
 
     return (swgptr)playerObj + 1432;
 }
@@ -227,7 +231,9 @@ Object* Game::getPlayerLookAtTargetObject()
     const swgptr lookAtId = getPlayerLookAtTargetObjectNetworkId();
 
     if (lookAtId == 0)
+    {
         return nullptr;
+    }
 
     return Object::getObjectById(lookAtId);
 }
@@ -241,7 +247,5 @@ const Camera* Game::getConstCamera()
 {
     return swg::game::getConstCamera();
 }
-
-
 
 }

UtinniCore/swg/game/game.h
@@ -35,5 +35,6 @@ public:
     static Camera* getCamera();
     static const Camera* getConstCamera();
 
+
 };
 }

UtinniCore/swg/ui/cui_hud.cpp
@@ -3,12 +3,13 @@
 #include "swg/misc/swg_misc.h"
 #include "swg/camera/camera.h"
 #include "swg/scene/client_world.h"
+#include "swg/scene/ground_scene.h"
 
 namespace swg::cuiHud
 {
 using pUpdate = void(__thiscall*)(swgptr pThis, float time);
 using pActionPerformAction = bool(__thiscall*)(swgptr pThis, DWORD val1, DWORD val2);
-using pGetTarget = utinni::Object* (__cdecl*)(utinni::Camera* pCamera, math::Vector* worldStart, math::Vector* worldEnd, utinni::Object* pObject);
+using pGetTarget = utinni::Object* (__cdecl*)(utinni::Camera* pCamera, math::Vector* worldStart, math::Vector* worldEnd, utinni::Object* obj);
 
 pUpdate update = (pUpdate)0x00BD56F0;
 pActionPerformAction actionPerformAction = (pActionPerformAction)0x00EDBAA0;
@@ -37,16 +38,16 @@ bool __fastcall hkActionPerformAction(swgptr pThis, DWORD EDX, DWORD val1, DWORD
 
 
 static swg::math::Vector cursorWorldPos;
-Object* __cdecl hkGetTarget(utinni::Camera* pCamera, swg::math::Vector* worldStart, swg::math::Vector* worldEnd, Object* pObject)
+Object* __cdecl hkGetTarget(utinni::Camera* pCamera, swg::math::Vector* worldStart, swg::math::Vector* worldEnd, Object* obj)
 {
     static constexpr uint16_t flags = (1 | 128 | 4096); // terrain | child objects | disable portal crossing
     CollisionInfo collisionResults;
-    if (clientWorld::collide(pCamera->getParentCell(), worldStart, worldEnd, collisionResults, flags, pObject))
+    if (clientWorld::collide(pCamera->getParentCell(), worldStart, worldEnd, collisionResults, flags, obj))
     {
         cursorWorldPos = swg::math::Vector(collisionResults.point);
     }
 
-    return swg::cuiHud::getTarget(pCamera, worldStart, worldEnd, pObject);
+    return swg::cuiHud::getTarget(pCamera, worldStart, worldEnd, obj);
 }
 
 static constexpr swgptr jmpToTarget_midUpdate = 0x00BD5961;
@@ -81,6 +82,26 @@ __declspec(naked) void midUpdate()
 
 const swg::math::Vector& getCursorWorldPosition()
 {
+    return cursorWorldPos;
+}
+
+swg::math::Vector collideCursorWithWorld(int x, int y)
+{
+    Camera* camera = GroundScene::get()->getCurrentCamera();
+    swg::math::Vector worldStart = camera->getTransform()->getPosition();
+    swg::math::Vector viewDirection = camera->getTransform_o2w()->rotate_l2p(camera->reverseProjectInViewportSpace(x - camera->viewportX, y - camera->viewportY));
+    viewDirection.normalize();
+
+    const float viewDistance = memory::read<float>(0x19488C8); // 0x19488C8 = Static location of view distance
+    swg::math::Vector worldEnd = worldStart + viewDirection * viewDistance;
+
+    Object* obj = nullptr;
+    static constexpr uint16_t flags = (1 | 128 | 4096); // terrain | child objects | disable portal crossing
+    CollisionInfo collisionResults;
+    if (clientWorld::collide(camera->getParentCell(), &worldStart, &worldEnd, collisionResults, flags, obj))
+    {
+        cursorWorldPos = swg::math::Vector(collisionResults.point);
+    }
     return cursorWorldPos;
 }
 

UtinniCore/swg/ui/cui_hud.h
@@ -6,6 +6,7 @@
 namespace utinni::cuiHud
 {
 UTINNI_API const swg::math::Vector& getCursorWorldPosition();
+UTINNI_API swg::math::Vector collideCursorWithWorld(int x, int y);
 UTINNI_API void patchAllowTargetEverything(bool value);
 
 void detour();

UtinniCore/swg/ui/cui_manager.h
@@ -21,6 +21,7 @@ class UTINNI_API UiManager
 public:
     static UiManager* get();
     void drawCursor(bool value);
+
 };
 
 class UTINNI_API SystemMessageManager

commit c407bf56bd0c27c5d58d34fdafe5135297e8f701

UtinniCore -- Fixed crash on scene change introduced in commit 91366e5

UtinniCore/swg/scene/ground_scene.cpp
@@ -104,13 +104,11 @@ void GroundScene::addUpdateLoopCallback(void(*func)(GroundScene* pThis, float el
 
 void __fastcall hkUpdateLoop(utinni::GroundScene* pThis, DWORD EDX, float time)
 {
-  
-    swg::groundScene::update(pThis, time);
     for (const auto& func : updateLoopCallbacks)
     {
         func(pThis, time);
     }
-
+    swg::groundScene::update(pThis, time);
 }
 
 void GroundScene::detour()

UtinniCore/swg/scene/world_snapshot.cpp
@@ -413,10 +413,10 @@ int WorldSnapshot::generateHighestId()
     return newId;
 }
 
-Object* createObject(WorldSnapshotReaderWriter::Node* mode)
+Object* createObject(WorldSnapshotReaderWriter::Node* node)
 {
     DWORD errorCode = 0;
-    return (Object*)swg::worldsnapshot::createObject(WorldSnapshotReaderWriter::get(), mode, errorCode);
+    return (Object*)swg::worldsnapshot::createObject(WorldSnapshotReaderWriter::get(), node, errorCode);
 }
 
 bool WorldSnapshot::isValidObject(const char* objectFilename)