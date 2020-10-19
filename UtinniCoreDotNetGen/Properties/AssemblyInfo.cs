8438c94ced35cbd9a758db4867bfb6fec9183f5c
ptklatt <timbab2@gmail.com>
2020-10-20 00:28:43 +0200

    Added basic versioning to most projects

--- UtinniCoreDotNetGen/Properties/AssemblyInfo.cs
+++ UtinniCoreDotNetGen/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Philip Klatt")]
 [assembly: AssemblyProduct("UtinniCoreDotNetGen")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020, Philip Klatt")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
