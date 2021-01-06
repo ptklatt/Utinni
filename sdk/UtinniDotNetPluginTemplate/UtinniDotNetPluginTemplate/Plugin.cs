f483a100942bbe4e2ae1219b8a4d2f4231c971ba
ptklatt <timbab2@gmail.com>
2021-01-06 21:46:58 +0100

    Removed example/ from the .sln, moved example/ to the new sdk/, WIP Plugin Templates in sdk/

---
+++ sdk/UtinniDotNetPluginTemplate/UtinniDotNetPluginTemplate/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using UtinniCore.Utinni;
+using UtinniCoreDotNet.PluginFramework;
+using UtinniCoreDotNet.Utility;
+
+namespace UtinniDotNetPluginTemplate
+{
+    public class Plugin : IPlugin
+    {
+        public Plugin()
+        {
+            Information = new PluginInformation("Example Plugin"
+                , "This is an Example Plugin"
+                , "ExampleAuthor");
+
+            Log.Info("Created: " + Information.Name);
+        }
+
+        public PluginInformation Information { get; }
+
+        public UtINI GetConfig()        
+        {
+            return null;
+        }
+    }
+}
